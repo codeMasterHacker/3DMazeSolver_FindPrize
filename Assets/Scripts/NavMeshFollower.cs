0d645fbeb79b823af3945e2cecfa4579d0d2657f
Enrique Munoz Jr <enrique.apple.id@gmail.com>
2021-07-01 23:20:57 -0700

    3D maze solver that uses nav mesh to find

---
+++ Assets/Scripts/NavMeshFollower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshFollower : MonoBehaviour
+{
+    public GameObject prize;
+    private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = prize.transform.position;
+    }
+}
